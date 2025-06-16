3aa5042d4c87fe73d123770b7f67bb3d68e83b2a
Koulahadjim Ghislain <gkoulastudy1@gmail.com>
2025-06-16 12:40:04 +0200

    correct

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -10,50 +10,52 @@ import ProjectCard from '../../common/ProjectCard';
 function Projects() {
   return (
     <section id='projects' className={styles.container}>
-        <h1 className='sectionTitle'>Projects</h1>
-        <div className={styles.projectsContainer}>
-          <ProjectCard 
-            src={viberr} 
-            link="https://github.com/AlchimistCode2/Graphic-Design1"
-            h3="Flyer Design"
-            p="Training Flyer"
-          />
+      <h1 className={styles.sectionTitle}>Projects</h1>
+      <div className={styles.projectsContainer}>
+        <ProjectCard 
+          src={viberr} 
+          link="https://github.com/AlchimistCode2/Graphic-Design1"
+          h3="Flyer Design"
+          p="Training Flyer"
+        />
 
-          <ProjectCard 
-            src={freshBurger} 
-            link="https://www.figma.com/proto/qWYQwVMGgGL1nQ18ywUN4Q/Based-gender-violence-Web-site?node-id=3-25&p=f&t=VEyxAJLaM6pVVklf-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1"
-            h3="Web design UI/X"
-            p="Ending gender violence based"
-          />  
-          <ProjectCard 
-            src={logoRealisation1} 
-            link="https://www.figma.com/proto/qWYQwVMGgGL1nQ18ywUN4Q/Based-gender-violence-Web-site?node-id=3-25&p=f&t=VEyxAJLaM6pVVklf-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1"
-            h3="Municipality's logo proposal"
-            p="Dynamique design for N'djamena city Municipality"
-          />  
-          <ProjectCard 
-            src={Banquier} 
-            link="https://www.figma.com/proto/qWYQwVMGgGL1nQ18ywUN4Q/Based-gender-violence-Web-site?node-id=3-25&p=f&t=VEyxAJLaM6pVVklf-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1"
-            h3="Logo competition"
-            p="The logo competition was launched by Réussir Begou"
-          />  
+        <ProjectCard 
+          src={freshBurger} 
+          link="https://www.figma.com/proto/qWYQwVMGgGL1nQ18ywUN4Q/Based-gender-violence-Web-site?node-id=3-25&p=f&t=VEyxAJLaM6pVVklf-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1"
+          h3="Web design UI/UX"
+          p="Ending gender violence based"
+        />  
 
-         <ProjectCard 
-            src={hipsster} 
-            link="https://github.com/AlchimistCode2/Graphic-Design1"
-            h3="Squadesign logo"
-            p="Logo design"
-          />
+        <ProjectCard 
+          src={logoRealisation1} 
+          link="https://www.figma.com/proto/qWYQwVMGgGL1nQ18ywUN4Q/Based-gender-violence-Web-site?node-id=3-25&p=f&t=VEyxAJLaM6pVVklf-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1"
+          h3="Municipality's logo proposal"
+          p="Dynamic design for N'Djamena city Municipality"
+        />  
 
-          <ProjectCard 
-            src={fitLift} 
-            link="https://github.com/AlchimistCode2/Ghislain_koula1"
-            h3="Portfolio"
-            p="Personnal portfolio"
-          />  
-        </div>
+        <ProjectCard 
+          src={Banquier} 
+          link="https://www.figma.com/proto/qWYQwVMGgGL1nQ18ywUN4Q/Based-gender-violence-Web-site?node-id=3-25&p=f&t=VEyxAJLaM6pVVklf-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1"
+          h3="Logo competition"
+          p="The logo competition was launched by Réussir Begou"
+        />  
+
+        <ProjectCard 
+          src={hipsster} 
+          link="https://github.com/AlchimistCode2/Graphic-Design1"
+          h3="Squadesign logo"
+          p="Logo design"
+        />
+
+        <ProjectCard 
+          src={fitLift} 
+          link="https://github.com/AlchimistCode2/Ghislain_koula1"
+          h3="Portfolio"
+          p="Personal portfolio"
+        />  
+      </div>
     </section>
   )
 }
 
-export default Projects
+export default Projects;
